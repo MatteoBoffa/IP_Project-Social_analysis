61b7dd898d6aa34fde9b22e5d7371579a18b2d88
MatteoBoffa <boffa.matteo.97@gmail.com>
2021-02-18 12:41:00 +0100

    Reviewed how the stats graphs were made

--- Classifying/Obtain_Stats.py
+++ Classifying/Obtain_Stats.py
@@ -8,8 +8,6 @@ from string import *
 INPUT_PATH = "./After_elaboration/Weekends_Only/Modified/"
 files = ["Flickr_final.xls", "Instagram_final.xls"]
 
-dict_classes = {}
-
 list_convertor = ["Palaces","Miscellaneous","Food","Facilities","Natural_views",\
 					"Animals","Shopping","Transportation","Urbanscape",\
 					"Exhibits","Religion","Residence","Entertainment","People"]
@@ -17,6 +15,9 @@ list_convertor = ["Palaces","Miscellaneous","Food","Facilities","Natural_views",
 dict_traslator = {}
 for it in range(len(list_convertor)):
 	dict_traslator[it+1]=list_convertor[it]
+
+dict_classes = {}
+dict_tot_photos = {}
 
 for file in files:
 	to_analyse = pd.read_csv(INPUT_PATH+file).to_numpy()
@@ -27,6 +28,9 @@ for file in files:
 			predictions = point[1].split(",")
 			if label not in list(dict_classes.keys()):
 				dict_classes[label] = {}
+				dict_tot_photos[label] = 1
+			else:
+				dict_tot_photos[label] += 1
 			for pred in predictions:
 				main_class = pred.split(":")[0].strip()
 				prob = float(pred.split(":")[1].strip())
@@ -39,8 +43,8 @@ for file in files:
 
 fig, ax= plt.subplots()
 zones = [int(el) for el in list(dict_classes.keys())]
-
-colors = ["#7D0000","#FF0000","#FB7756","#FDFA66","#1AC0C6",\
+#"#FDFA66"
+colors = ["#7D0000","#FF0000","#FB7756","gold","#1AC0C6",\
 			"#1A1B4B","#3E4491","#3A9EFD","#8C61FF","#DDACF5",\
 			"steelblue","#454D66","#58B368","#DAD873"]
 
@@ -66,10 +70,19 @@ for it,zone in enumerate(zones):
 
 	list_classes_ordered.sort(key = lambda x:x[0])
 
-	total = 0
 	for it2, el in enumerate(list_classes_ordered):
-		total+=el[1]
-		plt.barh(zone, el[1]/sum_class*100, left = left, color = dict_color[dict_traslator[int(el[0])]])
+		rect = ax.barh(zone, el[1]/sum_class*100, left = left, color = dict_color[dict_traslator[int(el[0])]])
+		if el[1]/sum_class*100 > 10:
+			for r in rect:
+				width, height =r.get_width(),r.get_height()
+				x=r.get_x()+width/2
+				y=r.get_y()+height/2
+				ax.annotate('{:.1f}%'.format(el[1]/sum_class*100), \
+					xy=(x,y), \
+					xytext=(0, 0), \
+					textcoords="offset points", \
+					ha='center', va='center', color = "w", size=10, weight='bold')
+
 		to_order.append((el[0],el[1]))
 		left = left + el[1]/sum_class*100
 
@@ -79,7 +92,7 @@ for it,zone in enumerate(zones):
 	else:
 		list_bars_per_zone.append(len(list_classes_ordered))
 
-	list_total_per_zone.append(total)	
+	list_total_per_zone.append(dict_tot_photos[zone])	
 
 	to_order.sort(key=lambda x: x[1], reverse=True)	
 	for it2,el in enumerate(to_order[:8]):
@@ -89,19 +102,21 @@ it_list_bars = 0
 
 for it,p in enumerate(ax.patches):
 	if (it+1) % list_bars_per_zone[it_list_bars] == 0:
-		percentage ='{:,.0f}'.format(list_total_per_zone[it_list_bars])
+		percentage ='{}'.format(list_total_per_zone[it_list_bars]).rjust(5,"\t").replace("\t","  ")
 		width, height =p.get_width(),p.get_height()
-		x=p.get_x()+width+0.45
-		y=p.get_y()+height/2-0.05
-		ax.annotate(percentage,(x,y), ha='left', va='center', size=10)
+		x=p.get_x()+width+0.5
+		y=p.get_y()+height/2
+		ax.annotate(percentage,(x,y), ha='left', va='center', size=16)
 		it_list_bars+=1
 
-plt.legend(list_convertor, ncol=7, loc='upper center', bbox_to_anchor=(0.5, 1.1), fontsize = 8)
+plt.legend(list_convertor, ncol=7, loc='upper center', bbox_to_anchor=(0.5, 1.125), fontsize = 10)
 ax2 = ax.twinx()
-ax2.set_ylabel('[photo taken]', fontsize = 16)
-ax.tick_params(labelsize=14)
-ax.set_xlabel("Percentages [%]", fontsize = 16)
-ax.set_ylabel("Zones", fontsize = 16)
+ax2.set_ylabel('[Photo taken]', fontsize = 20)
+ax.tick_params(labelsize=16)
+ax.set_xlabel("Percentages [%]", fontsize = 20)
+ax.set_ylabel("Zones", fontsize = 20)
 ax.set_yticks([x for x in range(len(zones))])
+ax2.margins(0.1)
 plt.setp(ax2.get_yticklabels(), visible=False)
+
 plt.show()
